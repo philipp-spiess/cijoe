3f24c2d0312355be089fc1a22bd70c73894c1b4f
Joshua Pinter <joshuapinter@gmail.com>
2012-02-29 00:22:13 -0700

    Removed humanize stuff.
    Relies on more Rails helpers. Whoops.

--- lib/cijoe/campfire.rb
+++ lib/cijoe/campfire.rb
@@ -57,27 +57,18 @@ class CIJoe
     end
 
     def short_message
-      "[#{humanize(build.project)}] #{(build.worked? ? "Passed" : "***FAILED***")} on #{build.branch} at #{build.short_sha} (#{build.duration.to_i}s)"
+      "[#{build.project}] #{(build.worked? ? "Passed" : "***FAILED***")} on #{build.branch} at #{build.short_sha} (#{build.duration.to_i}s)"
     end
 
     def full_message
       <<-EOM
+Commit Branch: #{build.branch}
 Commit Message: #{build.commit.message}
 Commit Date: #{build.commit.committed_at}
 Commit Author: #{build.commit.author}
 
 #{build.clean_output}
 EOM
-    end
-    
-    def humanize(lower_case_and_underscored_word)
-      result = lower_case_and_underscored_word.to_s.dup
-      inflections.humans.each { |(rule, replacement)| break if result.gsub!(rule, replacement) }
-      result.gsub!(/_id$/, "")
-      result.gsub!(/_/, ' ')
-      result.gsub(/([a-z\d]*)/i) { |match|
-        "#{inflections.acronyms[match] || match.downcase}"
-      }.gsub(/^\w/) { $&.upcase }
     end
   end
 end
